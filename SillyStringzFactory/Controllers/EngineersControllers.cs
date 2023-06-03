a4c1b895b5786396ceac001ca247bcc534c78c18
SierraRhodes <alreadywrote@gmail.com>
2023-06-02 17:47:56 -0700

    add create functionality to engineers

---
+++ SillyStringzFactory/Controllers/EngineersControllers.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SillyStringzFactory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SillyStringzFactory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly SillyStringzFactoryContext _db;
+
+    public EngineersController(SillyStringzFactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+     public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
