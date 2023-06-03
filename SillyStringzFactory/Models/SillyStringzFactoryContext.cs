01fce0a5b1a72f1bfc7230d6c27a0f6f989c75c1
SierraRhodes <alreadywrote@gmail.com>
2023-06-02 17:08:31 -0700

    add database context to models

---
+++ SillyStringzFactory/Models/SillyStringzFactoryContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SillyStringzFactory.Models
+{
+  public class SillyStringzFactoryContext : DbContext
+  {
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<Machine> Machines { get; set; }
+    public SillyStringzFactoryContext(DbContextOptions options) : base(options) { }
+  }
+}
